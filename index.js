abca137a5bc5bf41d8c5012a75ef0068659dcb28
Swarnim Srijan <1142srijan2002@gmail.com>
2024-05-13 13:43:35 +0530

    .

---
+++ index.js
@@ -0,0 +1,8 @@
+//let button = document.getElementById("button");
+let demo = document.getElementById("demo");
+function rexx(){
+    demo.innerHTML = "Hello there";
+    //this.innerHTML = Hello there;
+}
+console.log("Line 1\vLine 2"); // Outputs: Line 1 Line 2 (similar to '\n' in many environments)
+
